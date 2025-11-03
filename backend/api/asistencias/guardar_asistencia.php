dc895bdde11f86fe1eec71b7cd86082f4b503662
edunet2025 <admin@edunet.com.mx>
2025-11-02 18:42:54 -0600

    Update guardar_asistencia.php

--- backend/api/asistencias/guardar_asistencia.php
+++ backend/api/asistencias/guardar_asistencia.php
@@ -1,4 +1,9 @@
 <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 require_once __DIR__ . "/../../../conexion.php";
 date_default_timezone_set('America/Mexico_City');
